ef42a017899cb7bcfb186df337791227919709e1
= <=>
2025-03-13 12:56:10 +0100

    No idea. Last before rewrite

--- vrx_nav2/launch/vrx_nav2.launch.py
+++ vrx_nav2/launch/vrx_nav2.launch.py
@@ -47,10 +47,14 @@ def generate_launch_description():
         output='screen')
     
     # Does not work, must be launched in terminal manually as of now
+    # map_activate = ExecuteProcess(
+    #     cmd=['sleep', '60',';', 'ros2', 'lifecycle', 'set', '/map_server', 'configure;',
+    #          'sleep', '1', ';', 'ros2', 'lifecycle', 'set', '/map_server', 'activate'],
+    #     output='screen')
     map_activate = ExecuteProcess(
-        cmd=['sleep', '60',';', 'ros2', 'lifecycle', 'set', '/map_server', 'configure;',
-             'sleep', '1', ';', 'ros2', 'lifecycle', 'set', '/map_server', 'activate'],
-        output='screen')
+        cmd=[['sleep 10; ros2 lifecycle set /map_server configure; ros2 lifecycle set /map_server activate; ros2 run vrx_nav2 set_datum.py'
+        ]],
+        shell=True, output='screen')
 
     # Launch Nav2
     nav2_launch = IncludeLaunchDescription(
